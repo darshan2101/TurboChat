01b173ccd6fb6f25826139a84d9ee421d288d57e
Darshan <dgbabariya305@gmail.com>
2023-08-08 11:50:04 +0530

    devise controllers init

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -1,12 +1,14 @@
 class TurboDeviseController < ApplicationController
   class Responder < ActionController::Responder
     def to_turbo_stream
-      controller.render(options.merge(formats: :html))
+      rendering_options = options.merge(formats: :html)
+      controller.render(rendering_options)
     rescue ActionView::MissingTemplate => error
       if get?
         raise error
       elsif has_errors? && default_action
-        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+        rendering_options = rendering_options.merge(formats: :html, status: :unprocessable_entity)
+        controller.render(rendering_options)
       else
         redirect_to navigation_location
       end
