e7c2a411a95144d1e7d59b55fb267d382eb1ceb4
Darshan <dgbabariya305@gmail.com>
2023-12-06 16:58:05 +0530

    temp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-  after_action :set_status  
+  after_action :set_status
   def home
   end
-
+# temp
   private
     def set_status
       current_user.update!(status: User.statuses[:offline]) if current_user
